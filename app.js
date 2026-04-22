b3bd1045e019ca578a49acb5e6ba800b2329d438
Shumaila Riaz <shumailariaz215@gmail.com>
2026-04-22 17:38:07 +0500

    db connected

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
 const port = 5000;
 
+dotenv.config();
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send("Hello Homepage");
+  res.json("Server is running!");
 });
 
 app.listen(port, () => {
